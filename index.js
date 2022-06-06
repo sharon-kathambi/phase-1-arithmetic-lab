41c6484dcac0ae0da75b40e67cb554476da773e4
Sharon Mwenda <smkathambi@gmail.com>
2022-06-06 21:51:31 +0300

    commit tree fda8336ca5e3d5dce92d3e87d6ae3607483ca25e

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+ /*let 
+function multiply (num1, num2) {
+    num1.isInteger();
+    num2.isInteger();
+    return num1 * num2;
+}
+function random (Number){
+  return Number.isInteger(random);
+}*/
 
-// Write your code here
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = num1;
+const num4 = num2;
+const mod = num1 % num2;
+
+const max = Math.max(num1, num2, 20);
