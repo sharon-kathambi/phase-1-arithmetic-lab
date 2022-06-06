91f68c3d28547a8b914d7387b689469e9fab56eb
Sharon Mwenda <smkathambi@gmail.com>
2022-06-06 21:51:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = num1;
+const num4 = num2;
+const mod = num1 % num2;
+
+const max = Math.max(num1, num2, 20);
